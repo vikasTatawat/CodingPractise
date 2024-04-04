c3ca478f7fea69bb4058fbecfad5f5840ba46d49
Vikas <tatawatvikas7@gmail.com>
2024-04-04 22:45:27 +0530

    create an array

--- Arrays/arrayBasics.js
+++ Arrays/arrayBasics.js
@@ -12,7 +12,7 @@ console.log(strings[1]);
 
 // Adding an element at the last of the array
 
-console.log(string.push("e"));
+console.log(strings.push("e"));
 
 // Adding an element at the last of the array has O(1) time complexity because 
 // of the same reason, in this also we dont have to loop throught the elements
@@ -21,7 +21,7 @@ console.log(string.push("e"));
 
 // Deleting an element from the last of the array
 
-console.log(string.pop());
+console.log(strings.pop());
 // Deleting an element from the last of the array has O(1) time complexity because 
 // of the same reason, in this also we dont have to loop throught the elements
 
@@ -29,7 +29,7 @@ console.log(string.pop());
 
 // Adding a element at the start of the array
 
-console.log(string.unshift('x'));
+console.log(strings.unshift('x'));
 
 // Adding an element at the start on the array has O(n) time complexity because after adding the value
 // at the start, the memory will be allocated to the rest of the element again as the indexes
@@ -38,14 +38,55 @@ console.log(string.unshift('x'));
 
 // Adding a element at the middle of the array
 
-console.log(string.splice(3,0,'alien'));
+console.log(strings.splice(3,0,'alien'));
 
 // Adding an element at the middle on the array has O(n) time complexity because after adding the value
 // at the middle, the memory will be allocated to the rest of the element again as the indexes
 // of all the elements will be incremented by 1.
 
 
+// Build an array
+class MyArray {
+    constructor(){
+        this.length = 0;
+        this.data = {};
+    }
 
+    get(index){
+        return this.data[index];
+    }
+    push(item){
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+    pop(){
+        let lastItem = this.data[this.length - 1];
+        delete this.data[this.length-1];
+        this.length--;
+        return lastItem;
+    }
+    delete(index){
+        let item = this.data[index];
+        this.shiftItems(index);
+        return item;
+    }
+    shiftItems(index){
+        for (let i = index; i < this.length -1; i++) {
+           this.data[i] = this.data[i+1];
+        }
+        delete this.data[this.length - 1];
+    }
+}
+
+const newArray = new MyArray();
+newArray.push('hi');
+newArray.push('Vikas');
+
+newArray.push('Delete THis');
+newArray.pop();
+
+console.log(newArray);
 
 
 

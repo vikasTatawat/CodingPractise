a940f8480b5e9c4079d4f5c9f1bfaa363964b02d
Vikas <tatawatvikas7@gmail.com>
2025-01-08 21:38:18 +0530

    searching

--- Searching/binary.js
+++ Searching/binary.js
@@ -1,4 +1,4 @@
-let array =  [0, 1, 21, 33, 45, 45, 61, 71, 72, 73];
+let array = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73];
 let target = 33;
 
 function binarySearch(array, target) {
@@ -7,15 +7,12 @@ function binarySearch(array, target) {
   while (left <= right) {
     let mid = Math.ceil((left + right) / 2);
 
-    if (array[mid] == target) {
-      return mid;
-    } else if (array[mid] < target) {
-      left++;
+    if (array[mid] < target) {
+      left = mid + 1; // Move left to the right of mid
     } else if (array[mid] > target) {
-      right--;
+      right = mid - 1; // Move right to the left of mid
     }
   }
   return -1;
-
 }
 console.log(binarySearch(array, target));

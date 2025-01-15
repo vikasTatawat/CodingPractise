4a865542a94cbe65099b3ce00f3e299bc854f914
Vikas <tatawatvikas7@gmail.com>
2025-01-15 21:36:28 +0530

    bfs and dfs in graph

---
+++ Graphs/dfs.js
@@ -0,0 +1,20 @@
+class Node {
+    constructor(name) {
+      this.name = name;
+      this.children = [];
+    }
+  
+    addChild(name) {
+      this.children.push(new Node(name));
+      return this;
+    }
+  
+    depthFirstSearch(array) {
+        array.push(this.name);
+        for (const element of this.children) {
+            element.depthFirstSearch(array);
+        }
+        return array;
+        
+    }
+  }

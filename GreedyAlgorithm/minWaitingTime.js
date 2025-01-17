c8f37e69f73ff3d143ca15ce819368627ddd5718
Vikas <vikas.tatawat@simplilearn.com>
2025-01-17 22:10:18 +0530

    min waiting time

---
+++ GreedyAlgorithm/minWaitingTime.js
@@ -0,0 +1,14 @@
+let queries = [3, 2, 1, 2, 6];
+
+function minimumWaitingTime(queries) {
+  let sorted = queries.sort((a, b) => a - b); 
+  let duration = 0;
+  for (let i = 0; i < sorted.length; i++) {
+    let currentTotal = sorted[i];
+    let queriesLeft = sorted.length - (i + 1);
+    duration += currentTotal * queriesLeft;
+  }
+  return duration;
+}
+
+console.log(minimumWaitingTime(queries));
